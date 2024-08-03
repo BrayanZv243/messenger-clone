eac99d8fbda2fe614a4b38e3c1c9ca7ffeb6ba98
Brayan Zavala <brayan.zavala228311@potros.itson.edu.mx>
2024-08-03 15:06:12 -0700

    AuthUI

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { IconType } from "react-icons";
+import clsx from "clsx";
+
+interface AuthSocialButtonProps {
+    icon: IconType;
+    onClick: () => void;
+    disabled?: boolean;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+    icon: Icon,
+    onClick,
+    disabled,
+}) => {
+    console.log(disabled);
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            className={clsx(
+                `inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300`,
+                !disabled && "hover:bg-gray-50 focus:outline-offset-0",
+                disabled && "opacity-50"
+            )}
+        >
+            <Icon />
+        </button>
+    );
+};
+
+export default AuthSocialButton;
