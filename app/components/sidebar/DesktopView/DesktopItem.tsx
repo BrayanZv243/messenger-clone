5069d88491f63e770942c2a9aa63d7315720ffa3
Brayan Zavala <brayan.zavala228311@potros.itson.edu.mx>
2024-08-05 15:32:20 -0700

    Hint and tooltip

--- app/components/sidebar/DesktopView/DesktopItem.tsx
+++ app/components/sidebar/DesktopView/DesktopItem.tsx
@@ -2,6 +2,7 @@
 
 import clsx from "clsx";
 import Link from "next/link";
+import { Hint } from "../../Hint";
 
 interface DesktopItemProps {
     label: string;
@@ -23,16 +24,18 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
     };
     return (
         <li onClick={handleClick}>
-            <Link
-                href={href}
-                className={clsx(
-                    `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`,
-                    active && "bg-gray-100 text-black"
-                )}
-            >
-                <Icon className="h-6 w-6 shrink-0" />
-                <span className="sr-only">{label}</span>
-            </Link>
+            <Hint label={label} side="right" align="center">
+                <Link
+                    href={href}
+                    className={clsx(
+                        `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`,
+                        active && "bg-gray-100 text-black"
+                    )}
+                >
+                    <Icon className="h-6 w-6 shrink-0" />
+                    <span className="sr-only">{label}</span>
+                </Link>
+            </Hint>
         </li>
     );
 };
