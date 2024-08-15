ba28c89e43b743a916eb6e3f451e864182272b7e
Brayan Zavala <brayan.zavala228311@potros.itson.edu.mx>
2024-08-15 15:07:32 -0700

    w-11 and h-11 for all avatars on desktop and mobile version

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -14,8 +14,8 @@ const Avatar = ({ user }: AvatarProps) => {
     const isActive = members.indexOf(user?.email!) !== -1;
 
     return (
-        <div className="relative rounded-full h-9 w-9 md:h-11 md:w-11">
-            <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+        <div className="relative rounded-full h-11 w-11">
+            <div className="relative inline-block rounded-full overflow-hidden h-11 w-11">
                 {user?.image ? (
                     <Image
                         alt="Avatar"
@@ -30,7 +30,7 @@ const Avatar = ({ user }: AvatarProps) => {
                 )}
             </div>
             {isActive && (
-                <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
+                <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-3 w-3" />
             )}
         </div>
     );
